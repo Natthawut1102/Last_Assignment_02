c7191327b7fb75da04f699cc9a14e11610914950
Natthawut1102 <636051200088@mail.rmutk.ac.th>
2022-02-22 15:48:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/last_assignment_02/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.last_assignment_02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import com.example.last_assignment_02.Login
+import com.example.last_assignment_02.Member
+import com.example.last_assignment_02.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser != null){
+            val it = Intent(this, Member::class.java)
+            startActivity(it)
+            finish()
+        }
+
+        cmdLoginWithEmail.setOnClickListener {
+            val it = Intent(this, Login::class.java)
+            startActivity(it)
+        }
+    }
+}
